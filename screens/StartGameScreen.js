e8483df2870bc1686e73ff3e6357075aa058afc9
Azathot11 <87489449+Azathot11@users.noreply.github.com>
2023-03-07 09:58:52 +0100

    first commit

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { View,Text,TextInput } from 'react-native'
+
+const StartScreen = () => {
+  return (
+    <View>
+      <TextInput/>
+      <View></View>
+    </View>
+  )
+}
+
+
+const styles = StyleSheet.create({
+  
+});
+
+export default StartScreen
